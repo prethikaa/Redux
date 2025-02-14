0b2fc25b237294f61217983c064a3da8b79862f3
Hariharan Ravichandran <prethikaam@gmail.com>
2025-02-14 16:01:22 +0000

    Store and slice init

---
+++ src/movieSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  movies: [],
+};
+
+const movieSlice = createSlice({
+  name: "movies",
+  initialState,
+  reducers: {
+    addMovie: (state, action) => {
+      state.movies.push(action.payload);
+    },
+    removeMovie: (state, action) => {
+      state.movies.push(action.payload);
+    },
+  },
+});
+
+export const { addMovie, removeMovie } = movieSlice.actions;
+export default movieSlice.reducer;
